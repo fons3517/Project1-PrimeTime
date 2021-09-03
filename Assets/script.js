d0e45eb635b49598ee7a3d161ae296b15722f05a
Alfonso Robles <fons3517@gmail.com>
2021-09-02 20:38:00 -0500

    added index, js, css files. Added API key and 1 url

---
+++ Assets/script.js
@@ -0,0 +1,3 @@
+// Adding API key variable
+var APIkey = "iVixqY_0inkEjZubAm_PZMNO00butr5DY_YNMfoi0imSKdoICLpCh6IqOUAti2NyzT9L_3XwnlgOeIAptX6bnqAu0H7hFCplrjIT_hAB2W8vHPyUo_jnhsO_blIxYXYx";
+var queryURL1 = "https://api.yelp.com/v3/businesses/search";
