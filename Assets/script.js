348a5b4f4cdc04f4b8d9323423e96515fe13c816
Jose Izquierdo <leftjose@gmail.com>
2021-09-12 22:40:48 -0500

    merging files

--- Assets/script.js
+++ Assets/script.js
@@ -96,3 +96,15 @@ function createMarker(place) {
  localStorage.setItem('googleAPIkey', googleAPIkey)
  localStorage.getItem(googleAPIkey)
 
+
+//  document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems, options);
+// });
+
+// // Or with jQuery
+
+// $('.dropdown-trigger').dropdown();
+
+
+// var instance = M.Dropdown.getInstance(elem);
